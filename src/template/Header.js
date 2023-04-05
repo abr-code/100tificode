26593e8bb4c916f31e46a955cfc70a7bfe361f5c
abr-code <abraham.ortega.dev@gmail.com>
2023-04-05 09:52:49 -0500

    🎉 initial: commit to upload project to github

---
+++ src/template/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  const view = `
+    <div class="Header-main">
+      <div class="header-logo">
+        <h1>
+          <a href="/">100tifi.co</a>
+        </h1>
+      </div>
+      <div class="Header-nav">
+      <a href="#/abaut/">Abaut</a>
+      </div>
+    </div>
+    `;
+  return view;
+};
+
+export default Header;
